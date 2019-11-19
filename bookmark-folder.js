55e102ae4981035e5dacec679c92ce4b71405485
Ken <ken75113@gmail.com>
2019-11-19 18:00:32 +0800

    Show bookmark tree on right panel

--- bookmark-folder.js
+++ bookmark-folder.js
@@ -1,5 +1,8 @@
 const init = async function() {
   renderFolderTree();
+
+  renderBookmarkTree();
+  window.addEventListener('hashchange', renderBookmarkTree)
 };
 
 async function renderFolderTree() {
@@ -15,14 +18,62 @@ function createFolderTree(node) {
 
     // Create list item
     const li = document.createElement('li');
-    const span = document.createElement('span');
-    span.textContent = child.title;
-    li.appendChild(span);
+    const anchor = document.createElement('a');
+    anchor.href = '#'+child.id;
+    anchor.textContent = child.title;
+    li.appendChild(anchor);
     ul.appendChild(li);
 
     // Create child list
     const subTree = createFolderTree(child);
     if (subTree.hasChildNodes()) li.appendChild(subTree);
+  }
+  return ul;
+}
+
+async function renderBookmarkTree() {
+  document.querySelector('#bookmarkTree').innerHTML = '';
+  if (location.hash=='') {
+    document.title = 'Hold My Tabs';
+    document.querySelector('#folderTitle').innerHTML = '';
+  } else {
+    const folderId = location.hash.substring(1);
+    const subTree = (await browser.bookmarks.getSubTree(folderId))[0];
+    document.querySelector('#folderTitle').textContent = subTree.title;
+    document.title = subTree.title + ' - Hold My Tabs';
+    const bookmarkTree = createBookmarkTree(subTree);
+    document.querySelector('#bookmarkTree').appendChild(bookmarkTree);
+  }
+}
+
+function createBookmarkTree(node) {
+  const ul = document.createElement('ul');
+  for (child of node.children) {
+    // Create list item
+    const li = document.createElement('li');
+    if (child.type == 'separator' || child.url=='data:') { // Separator
+      const span = document.createElement('span');
+      span.textContent = '--------------------------------';
+      li.appendChild(span);
+    } else if (child.url) { // Bookmark
+      const anchor = document.createElement('a');
+      anchor.href = child.url;
+      anchor.target = '_blank';
+      anchor.textContent = child.title;
+      li.appendChild(anchor);
+    } else { // Folder
+      const anchor = document.createElement('a');
+      anchor.href = '#'+child.id;
+      anchor.textContent = child.title;
+      li.appendChild(anchor);
+    }
+    ul.appendChild(li);
+
+    // Create child list
+    if (!child.url) {
+      const subTree = createBookmarkTree(child);
+      if (subTree.hasChildNodes()) li.appendChild(subTree);
+    }
   }
   return ul;
 }
