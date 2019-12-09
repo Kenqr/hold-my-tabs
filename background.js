767ed041698ddbf4b4cdaeffcfd54524777b77ff
Ken <ken75113@gmail.com>
2019-12-09 18:29:15 +0800

    Reindent background.js

--- background.js
+++ background.js
@@ -1,84 +1,84 @@
 browser.browserAction.onClicked.addListener(async function(){
-    // Get current tab
-    const currentTab = (await browser.tabs.query({currentWindow: true, active: true}))[0];
-
-    const hmtTab = await findClosestHmtTabOnLeft(currentTab.index);
-
-    if (hmtTab) {
-        // Get folder data
-        const folder = await getFolderFromHmtTab(hmtTab);
-
-        try {
-            moveTabToFolder(currentTab, folder);
-        } catch (e) {
-            // Do nothing
-        }
-    } else {
-        // Open extension page
-        browser.tabs.create({
-            url: "/bookmark-folder.html",
-            index: currentTab.index, // Previous to current tab
-        });
+  // Get current tab
+  const currentTab = (await browser.tabs.query({currentWindow: true, active: true}))[0];
+  
+  const hmtTab = await findClosestHmtTabOnLeft(currentTab.index);
+  
+  if (hmtTab) {
+    // Get folder data
+    const folder = await getFolderFromHmtTab(hmtTab);
+    
+    try {
+      moveTabToFolder(currentTab, folder);
+    } catch (e) {
+      // Do nothing
     }
+  } else {
+    // Open extension page
+    browser.tabs.create({
+      url: "/bookmark-folder.html",
+      index: currentTab.index, // Previous to current tab
+    });
+  }
 });
 
 async function findClosestHmtTabOnLeft(currentTabIndex) {
-    // Get extension page url
-    const url = browser.runtime.getURL('bookmark-folder.html');
-
-    // Get HMT tabs in current window
-    const tabList = await browser.tabs.query({
-        currentWindow: true,
-        url: [
-            url,
-            url + '#*',
-        ],
-    });
-
-    // Find tab with largest index but smaller than currentTab
-    for (let i=tabList.length-1; i>=0; i--) {
-        if (tabList[i].index < currentTabIndex) return tabList[i];
-    }
-
-    return null;
+  // Get extension page url
+  const url = browser.runtime.getURL('bookmark-folder.html');
+  
+  // Get HMT tabs in current window
+  const tabList = await browser.tabs.query({
+    currentWindow: true,
+    url: [
+      url,
+      url + '#*',
+    ],
+  });
+  
+  // Find tab with largest index but smaller than currentTab
+  for (let i=tabList.length-1; i>=0; i--) {
+    if (tabList[i].index < currentTabIndex) return tabList[i];
+  }
+  
+  return null;
 }
 
 async function getFolderFromHmtTab(hmtTab) {
-    const url = new URL(hmtTab.url);
-    const folderId = url.hash.substring(1);
-
-    if (folderId) {
-        return (await browser.bookmarks.getSubTree(folderId))[0];
-    } else {
-        return null;
-    }
+  const url = new URL(hmtTab.url);
+  const folderId = url.hash.substring(1);
+  
+  if (folderId) {
+    return (await browser.bookmarks.getSubTree(folderId))[0];
+  } else {
+    return null;
+  }
 }
 
 /**
- * Check if folder contains a direct child with url.
- *
- * @param {bookmarks.BookmarkTreeNode} folder
- * @param {string} url 
- * @returns {boolean}
- */
+* Check if folder contains a direct child with url.
+*
+* @param {bookmarks.BookmarkTreeNode} folder
+* @param {string} url 
+* @returns {boolean}
+*/
 function folderHasChildWithUrl(folder, url) {
-    for (const child of folder.children) {
-        if (child.url === url) return true;
-    }
-    return false;
+  for (const child of folder.children) {
+    if (child.url === url) return true;
+  }
+  return false;
 }
 
 async function moveTabToFolder(tab, folder) {
-    // Bookmark the tab if it is not already in the folder
-    if (!folderHasChildWithUrl(folder, tab.url)) {
-        await browser.bookmarks.create({
-            parentId: folder.id,
-            index: folder.children.length,
-            title: tab.title,
-            url: tab.url,
-        });
-    }
-
-    // Close the tab
-    return browser.tabs.remove(tab.id);
+  // Bookmark the tab if it is not already in the folder
+  if (!folderHasChildWithUrl(folder, tab.url)) {
+    await browser.bookmarks.create({
+      parentId: folder.id,
+      index: folder.children.length,
+      title: tab.title,
+      url: tab.url,
+    });
+  }
+  
+  // Close the tab
+  return browser.tabs.remove(tab.id);
 }
