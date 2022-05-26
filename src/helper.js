5d81e5cb38a662800356953cfdf4f2482d2d71dc
Ken <ken75113@gmail.com>
2022-05-26 17:42:56 +0800

    Add $create() for creating elements

--- src/helper.js
+++ src/helper.js
@@ -1,2 +1,27 @@
 // Alias for document.querySelector
 export const $ = (...args) => document.querySelector(...args);
+
+/**
+ * Create a new element
+ * @param {string} tag - The name of the element
+ * @param {object} attrs - Attributes of the element
+ * @param {array} children - Child elements to be created recursively
+ * @returns The new element
+ */
+export const $create = ([tag, attrs = {}, ...children]) => {
+  // Create the base element
+  const elem = document.createElement(tag);
+
+  // Set attributes
+  Object.getOwnPropertyNames(attrs).forEach(name => {
+    elem.setAttribute(name, attrs[name]);
+  });
+
+  // Create children
+  children.map(child => {
+    if (typeof child === 'object') return $create(child);
+    return document.createTextNode(child);
+  }).forEach(node => elem.appendChild(node));
+
+  return elem;
+};
